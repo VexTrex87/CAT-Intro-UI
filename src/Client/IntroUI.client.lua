63a22acc01f0c8d6fda70237b214cd4f8deb0080
VexTrex <66855203+Vex87@users.noreply.github.com>
2020-11-08 22:06:26 -0500

    finished hover & clicked events

--- src/Client/IntroUI.client.lua
+++ src/Client/IntroUI.client.lua
@@ -6,7 +6,11 @@ end
 
 -- // Variables \ --
 
-local FADE_TWEEN_INFO = TweenInfo.new(0.5)
+local FADE_TWEEN_INFO = TweenInfo.new(0.3)
+local MOVE_TWEEN_INFO = TweenInfo.new(0.3)
+local INTRO_FADE_TWEEN_INFO = TweenInfo.new(1)
+local HOVERED_POS = UDim2.new(0, 0, 0, 0)
+local UNHOVERED_POS = UDim2.new(-0.01, 0, 0, 0)
 
 local Core = require(game.ReplicatedStorage.Modules.Core)
 local NewTween = Core("newTween")
@@ -22,6 +26,7 @@ local function onFrameHovered(frame)
     local fullImage = background:FindFirstChild(frame.Name .. "-Full")
     NewTween(transImage, FADE_TWEEN_INFO, {ImageTransparency = 1})
     NewTween(fullImage, FADE_TWEEN_INFO, {ImageTransparency = 0})
+    NewTween(fullImage, MOVE_TWEEN_INFO, {Position = HOVERED_POS})
 end
 
 local function onFrameUnhovered(frame)
@@ -29,19 +34,29 @@ local function onFrameUnhovered(frame)
     local fullImage = background:FindFirstChild(frame.Name .. "-Full")
     NewTween(transImage, FADE_TWEEN_INFO, {ImageTransparency = 0})
     NewTween(fullImage, FADE_TWEEN_INFO, {ImageTransparency = 1})
+    NewTween(fullImage, MOVE_TWEEN_INFO, {Position = UNHOVERED_POS})
+end
+
+local function onFrameClicked(frame)
+    background.Visible = false
+    print("changing team to " .. frame.Name)
 end
 
 -- // COMPILE \\ --
 
+game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Health, false)
 background.Visible = true
 
 for _,frame in pairs(background:GetChildren()) do
-    if frame:IsA("Frame") then
+    if frame:IsA("ImageButton") then
         frame.MouseEnter:Connect(function()
             onFrameHovered(frame)
         end)
         frame.MouseLeave:Connect(function()
             onFrameUnhovered(frame)
+        end)
+        frame.MouseButton1Click:Connect(function()
+            onFrameClicked(frame)
         end)
     end
 end
